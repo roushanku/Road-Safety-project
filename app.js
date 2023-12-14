d72a4a85ece5feef52d1d46eaa359996dc3632b9
roushanku <roushankuiitbhilai.ac.in>
2023-12-14 20:56:07 +0530

    implented post api

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const  eventController = require('./controllers/eventController')
-// const cors = require('cors');
 
 const app = express();
 const PORT = 8080;
 
 app.use(express.json());
-// app.use(cors());
 
 app.get('/event',eventController.getEvent)
+
+app.post('/alert/:alertId',eventController.addEvent)
 
 app.listen(PORT,()=>console.log(`Server started on port ${PORT}...`))
 
--- controllers/eventController.js
+++ controllers/eventController.js
@@ -1,5 +1,25 @@
+let eventList = []
 const getEvent = async function (req , res){
-    res.send('Welcome to server')
+
+    res.send('Welcome to server' , {
+        "timestamp": Date,
+        "is_overspeeding": true,
+        "vehicle_id": 1234,
+        "location_type": "highway"
+    })
 }
 
-module.exports = {getEvent}
+const addEvent = async function (req , res){
+    let event =
+        {
+            "timestamp": Date,
+            "is_overspeeding": true,
+            "vehicle_id": 1234,
+            "location_type": "highway"
+        }
+    eventList.push(event)
+    // console.log(req.params.alertId)
+    res.send('Event added successfully' , event)
+}
+
+module.exports = {getEvent , addEvent}
